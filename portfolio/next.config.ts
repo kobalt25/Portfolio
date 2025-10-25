abd5f97d4a53a6b88d997dbc1e207fb141eb2816
kobalt25 <oswiba@outlook.com>
2025-10-26 00:00:24 +0100

    Removed build config

--- portfolio/next.config.ts
+++ portfolio/next.config.ts
@@ -1,16 +1,7 @@
 import type { NextConfig } from 'next'
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
-    reactStrictMode: true,
-    images:{
-        unoptimized: true,
-    },
-    assetPrefix: isProd ? '/kobalt25.github.io' : '',
-    basePath: isProd ? '/kobalt25.github.io' : '',
-    output: 'export'
-    /* config options here */
+
 }
 
 export default nextConfig
